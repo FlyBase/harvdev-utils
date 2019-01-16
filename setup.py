c2dff416077188bb5a8aced8df1aea64e638ce34
Chris Tabone <christabone@gmail.com>
2019-01-16 18:00:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'harvdev_utils',
     version = '1.0.0',
-    url = 'https://github.com/FlyBase/harvdev_utils',
+    url = 'https://github.com/FlyBase/harvdev-utils',
     author = 'FlyBase Harvard Developers',
     author_email = 'harvdev@morgan.harvard.edu',
     description = 'Common Python functions used by FlyBase developers at Harvard.',
