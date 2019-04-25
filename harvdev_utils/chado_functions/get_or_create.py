2853e614e72be62b237a1c5db8683aec5c93c247
Christopher Tabone <christabone@gmail.com>
2019-04-25 18:49:13 -0300

    Added docs and tweaked one debug line in get or create.

--- harvdev_utils/chado_functions/get_or_create.py
+++ harvdev_utils/chado_functions/get_or_create.py
@@ -47,7 +47,6 @@ def get_or_create(session, model, ret_col=None, **kwargs):
     else:
         try:
             attempt = session.query(model).filter_by(**kwargs).one()
-            log.debug(attempt)
             log.debug('Found previous entry for %s, insert not required.' % (kwargs))
             if ret_col is not None: # If we're expecting a returning value from the Postgres insert.
                 return getattr(attempt, ret_col) # Return this new value from our recently checked object.
