61eb84d48c38cb6ec1f6d12681dc5dd0df4b1ea6
ianlongden <ianlongden@morgan.harvard.edu>
2020-01-29 12:40:05 -0500

    add new modules

--- harvdev_utils/__init__.py
+++ harvdev_utils/__init__.py
@@ -1,5 +1,8 @@
-from .char_conversions import *
-from .chado_functions import *
+from .char_conversions import (
+    sgml_to_plain_text, sgml_to_unicode,
+    unicode_to_plain_text, sub_sup_to_sgml
+)
+from .chado_functions import get_or_create, get_cvterm, chado_errors
 
 # if somebody does "from somepackage import *", this is what they will
 # be able to access:
@@ -8,5 +11,7 @@ __all__ = [
     'sgml_to_unicode',
     'unicode_to_plain_text',
     'sub_sup_to_sgml',
-    'get_or_create'
+    'get_or_create',
+    'get_cvterm',
+    'chado_errors'
 ]
---
+++ harvdev_utils/chado_functions/cvterm.py
@@ -0,0 +1,29 @@
+# store easy lookup for cv, cvterm lookups
+# should save time in the long run
+from ..production import Cv, Cvterm
+from sqlalchemy.orm.exc import NoResultFound
+from .chado_errors import CodingError
+cv_cvterm = {}
+
+
+def get_cvterm(session, cv_name, cvterm_name):
+    """
+    Lookup cvterm.
+    """
+    global cv_cvterm
+    try:
+        return cv_cvterm[cv_name][cvterm_name]
+    except KeyError:
+        pass
+    try:
+        cvterm = session.query(Cvterm).join(Cv).\
+            filter(Cvterm.name == cvterm_name,
+                   Cv.name == cv_name,
+                   Cvterm.is_obsolete == 0).one()
+        if cv_name not in cv_cvterm:
+            cv_cvterm[cv_name] = {}
+        cv_cvterm[cv_name][cvterm_name] = cvterm
+    except NoResultFound:
+        raise CodingError("HarvdevError: Could not find cv {}, cvterm {}.".format(cv_name, cvterm_name))
+        return None
+    return cv_cvterm[cv_name][cvterm_name]
