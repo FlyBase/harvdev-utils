92e1d6073f1454c769d3b6bae00d74d5d41888f6
gildossantos <dossantos@morgan.harvard.edu>
2020-02-05 15:32:31 -0500

    update init

--- harvdev_utils/char_conversions/__init__.py
+++ harvdev_utils/char_conversions/__init__.py
@@ -3,3 +3,4 @@ from .sgml_to_unicode import sgml_to_unicode
 from .unicode_to_plain_text import unicode_to_plain_text
 from .greek_to_sgml import greek_to_sgml
 from .sub_sup_to_sgml import sub_sup_to_sgml
+from .sub_sup_sgml_to_html import sub_sup_sgml_to_html
