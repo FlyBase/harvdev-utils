77e4df11570e27142283224faa0891d08a74e88a
ianlongden <ianlongden@morgan.harvard.edu>
2020-02-11 08:38:01 -0500

    New error to specify a data error.

--- harvdev_utils/chado_functions/chado_errors.py
+++ harvdev_utils/chado_functions/chado_errors.py
@@ -8,3 +8,15 @@ class CodingError(Exception):
 
     def __init__(self, message):
         self.error = message
+
+
+class DataError(Exception):
+    """Exception raised for errors due to data problems.
+       Example: fetching one_or_none and getting many results.
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.error = message
