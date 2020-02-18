479bff350a4bd0d0256915a2252430cedbf1e464
gildossantos <dossantos@morgan.harvard.edu>
2020-02-18 14:42:45 -0500

    module for writing proforma records from a given list of dicts

--- harvdev_utils/general_functions/__init__.py
+++ harvdev_utils/general_functions/__init__.py
@@ -4,6 +4,10 @@ from .today import today
 from .count_value_frequency import count_value_frequency
 from .generic_FB_tsv_dict import generic_FB_tsv_dict
 from .generic_AGR_json_dict import generic_AGR_json_dict
-from .dump_data_to_file import check_data_object
-from .dump_data_to_file import tsv_report_dump
-from .dump_data_to_file import json_dump
+from .dump_data_to_file import (
+    check_data_object, tsv_report_dump, json_dump
+)
+from .write_proforma import (
+    get_proforma_masters, get_distinct_proforma_field_prefixes, detect_proforma_type, write_record_curation_header,
+    write_record_end, write_proforma_line, write_proforma_stanza, write_proforma_record
+)
