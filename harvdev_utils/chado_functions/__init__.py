605da16733fad79e4d3c41e9484221d0ea7c8f1a
ianlongden <ianlongden@morgan.harvard.edu>
2020-02-26 11:25:09 -0500

    add function to get db chado object from name with cache

--- harvdev_utils/chado_functions/__init__.py
+++ harvdev_utils/chado_functions/__init__.py
@@ -1,4 +1,5 @@
 from .get_or_create import get_or_create
 from .external_lookups import ExternalLookup
 from .cvterm import get_cvterm
+from .db import get_db
 from .chado_errors import CodingError, DataError
