d8998ad53dc164401833e1bd52845fd52832da29
ianlongden <ianlongden@morgan.harvard.edu>
2020-02-26 11:26:35 -0500

    add function to get db chado object from name with cache

---
+++ harvdev_utils/chado_functions/db.py
@@ -0,0 +1,23 @@
+# store easy lookup for db's
+# should save time in the long run
+from ..production import Db
+from sqlalchemy.orm.exc import NoResultFound
+from .chado_errors import CodingError
+db_dict = {}
+
+
+def get_db(session, db_name):
+    """Lookup dn chsado object given name."""
+    global db_dict
+    try:
+        return db_dict[db_name]
+    except KeyError:
+        pass
+    try:
+        db = session.query(Db).filter(Db.name == db_name).one()
+        if db:
+            db_dict[db_name] = db
+    except NoResultFound:
+        raise CodingError("HarvdevError: Could not find db {}.".format(db_name))
+        return None
+    return db_dict[db_name]
