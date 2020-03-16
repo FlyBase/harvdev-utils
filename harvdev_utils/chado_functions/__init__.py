f57476a5966f2c724de50f3c8a604338c8764039
Chris Tabone <ctabone@morgan.harvard.edu>
2020-03-16 15:24:37 -0400

    Updated __init__.py

--- harvdev_utils/chado_functions/__init__.py
+++ harvdev_utils/chado_functions/__init__.py
@@ -1,4 +1,5 @@
 from .get_or_create import get_or_create
+from .get_update_or_create import get_update_or_create
 from .external_lookups import ExternalLookup
 from .cvterm import get_cvterm
 from .db import get_db
