9c0b45b0617b5d9398c11a69a498c6cd592d15e5
Ian Longden <ilongden@Ians-MacBook-Pro.local>
2020-04-23 17:09:51 -0400

    Add get_dbxref

--- harvdev_utils/chado_functions/__init__.py
+++ harvdev_utils/chado_functions/__init__.py
@@ -3,7 +3,9 @@ from .get_or_create import get_or_create
 from .get_create_or_update import get_create_or_update
 from .external_lookups import ExternalLookup
 from .cvterm import get_cvterm
-from .db import get_db
+from .db import (
+    get_db, get_dbxref
+)
 from .chado_errors import CodingError, DataError
 from .synonym import synonym_name_details
 from .organism import (
