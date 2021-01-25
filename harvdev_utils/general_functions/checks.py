855ca1cd849c76af7c1dfc98ff51824306f5ed1e
ianlongden <ianlongden@morgan.harvard.edu>
2021-01-25 17:11:49 -0500

    Add new check functions

---
+++ harvdev_utils/general_functions/checks.py
@@ -0,0 +1,136 @@
+"""Check for proforma log and xml files."""
+from collections import deque
+import re
+
+
+def xort_logfile_check(filename):
+    """Check output from xort.
+
+    filename: full filename and path of xort log file.
+              will normally be something like
+              /users/falls/DB_update/epicycle_log/harv_2017_02_e5_p1.load
+
+    return wether okay or not and an error string
+    """
+    okay = True
+    errors = []
+
+    # check log file for the word ERROR
+    count = 0
+    old_lines = deque(maxlen=10)
+    count = 0
+    bingo_you_success = False
+    with open(filename, "r") as log_file:
+        for line in log_file:
+            count += 1
+            old_lines.append(line)
+            if "bingo ....you success" in line:
+                bingo_you_success = True
+            if 'ERROR' in line:
+                okay = False
+                errors.append((count, filename, old_lines))
+    error_lines = ""
+    if not okay:
+        for item in errors:
+            error_lines += "file:{}, line {}, 10 subsequent lines were {}".format(item[1], item[0], item[2])
+
+    if not count:  # Must be a problem if empty
+        error_lines = "Empty log file.This should not be possible!!"
+        okay = False
+    if not bingo_you_success:
+        error_lines = "Error: NO 'bingo ....you success' found"
+        okay = False
+    return okay, error_lines
+
+
+def proforma_logfile_check(logfile):
+    """Check proforma output.
+
+    1) check logfile for string ERROR
+       if found get file and lines around error i.e.
+       /users/falls/DB_update/epicycle_log/harv_2017_02_e5_p1.log
+
+    return wether okay or not and an error string
+    """
+    okay = True
+    errors = []
+    proforma_file_pattern = r'==Opening: ./proforma/(\S+)=='
+
+    # check log file for the word ERROR
+    old_lines = deque(maxlen=10)
+    count = 0
+    file_filename = None
+    with open(logfile, "r") as log_file:
+        for line in log_file:
+            count += 1
+            old_lines.append(line)
+            fields = re.search(proforma_file_pattern, line)
+            if fields:
+                if fields.group(1):
+                    file_filename = fields.group(1)
+            if 'ERROR' in line:
+                okay = False
+                output = ''
+                for old_line in old_lines:
+                    output += "\tline: {}".format(old_line)
+                errors.append((count, file_filename, output))
+    error_lines = ""
+    if not okay:
+        for item in errors:
+            error_lines += "file:{}, line {}, 10 subsequent lines were:-\n{}".format(item[1], item[0], item[2])
+
+    if not count:  # Must be a problem if empty
+        error_lines += "Empty log file.This should not be possible!!"
+        okay = False
+    if not file_filename:
+        error_lines += "No file in log file. So no files processed?"
+        okay = False
+
+    return okay, error_lines
+
+
+def check_xml(xmlfile):
+    """Check xml ends in </chado>.
+
+    return wether okay or not and an error string
+    """
+    okay = True
+    error = ""
+    line_count = 0
+    # check xml ends with </chado>
+    with open(xmlfile) as xml_file:
+        for line in xml_file:
+            line_count += 1
+        if not line_count:
+            okay = False
+            error = "Empty file not allowed"
+        elif '</chado>' not in line:
+            error = "ERROR: {} does not end in '</chado>' but with '{}'.".format(xmlfile, line)
+            okay = False
+    return okay, error
+
+
+def new_proforma_log_check(filename):
+    """Check this log file to make and pass back success status.
+
+    return wether okay or not and an error string
+    """
+    okay = True
+
+    # check log file for the word ERROR
+    count = 0
+    error_lines = ""
+    bingo_you_success = False
+    with open(filename, "r") as log_file:
+        for line in log_file:
+            count += 1
+            if "bingo ....you success" in line:
+                bingo_you_success = True
+
+    if not count:  # Must be a problem if empty
+        error_lines += "Empty log file. No data??\n"
+        okay = False
+    if not bingo_you_success:
+        error_lines += "Error: NO 'bingo ....you success' found\n"
+        okay = False
+    return okay, error_lines
---
+++ tests/test_general_functions/test_checks.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `harvdev_utils` package checks.py file."""
+import unittest
+
+from harvdev_utils.general_functions import (
+    xort_logfile_check, proforma_logfile_check, check_xml, new_proforma_log_check
+)
+
+def test_xml_good():
+    # will not get here unless all the module are installed.
+    okay, error_string = check_xml("./tests/test_general_functions/test_data_files/good_test.xml")
+    assert okay
+    assert error_string == ""
+
+def test_xml_bad():
+    # will not get here unless all the module are installed.
+    okay, error_string = check_xml("./tests/test_general_functions/test_data_files/bad_test.xml")
+    assert not okay
+    assert "does not end in '</chado>'" in error_string
+
+def test_xml_empty():
+    # Test we get an error and message lives up to that.
+    okay, error_string = check_xml("./tests/test_general_functions/test_data_files/empty_test.xml")
+    assert not okay
+    assert "Empty" in error_string
+
+def test_old_proforma_good():
+    # will not get here unless all the module are installed.
+    okay, error_string = proforma_logfile_check("./tests/test_general_functions/test_data_files/old_proforma_good.log")
+    assert okay
+    assert error_string == ""
+
+def test_old_proforma_bad():
+    # will not get here unless all the module are installed.
+    okay, error_string = proforma_logfile_check("./tests/test_general_functions/test_data_files/old_proforma_bad.log")
+    assert not okay
+    assert "file:al2794.edit" in error_string
+
+def test_xml_empty():
+    # Test we get an error and message lives up to that.
+    okay, error_string = proforma_logfile_check("./tests/test_general_functions/test_data_files/old_proforma_empty.log")
+    assert not okay
+    assert "Empty" in error_string
