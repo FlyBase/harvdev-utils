c8aa03f7096bdf54a097e467f7e3745b65ef2c34
gildossantos <gilbertodossantos74@gmail.com>
2022-09-07 12:34:11 -0400

    add new fx to init

--- harvdev_utils/char_conversions/__init__.py
+++ harvdev_utils/char_conversions/__init__.py
@@ -5,3 +5,4 @@ from .unicode_to_plain_text import unicode_to_plain_text
 from .greek_to_sgml import greek_to_sgml
 from .sub_sup_to_sgml import sub_sup_to_sgml
 from .sub_sup_sgml_to_html import sub_sup_sgml_to_html
+from .sub_sup_sgml_to_plain_text import sub_sup_sgml_to_plain_text
